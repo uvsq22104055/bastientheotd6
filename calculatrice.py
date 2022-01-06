5c57f0ff4aac4ad615ccee5a8232d1ac813bf516
Theo JOLY <theo.joly@ens.uvsq.fr>
2022-01-06 10:50:24 +0100

    m

--- calculatrice.py
+++ calculatrice.py
@@ -5,31 +5,33 @@ racine = tk.Tk()
 racine.title("interface td5")
 
 bouton_1 = tk.Button(racine, text="1")
-bouton_1.grid(row=1, column=0)
+bouton_1.grid(row=2, column=1)
 
 bouton_2 = tk.Button(racine, text="2")
-bouton_2.grid(row=2, column=0)
+bouton_2.grid(row=2, column=2)
 
 bouton_3 = tk.Button(racine, text="3")
-bouton_3.grid(row=2, column=0)
+bouton_3.grid(row=2, column=3)
 
-bouton_4 = tk.Button(racine, text="2")
-bouton_4.grid(row=2, column=0)
+bouton_4 = tk.Button(racine, text="4")
+bouton_4.grid(row=3, column=1)
 
-bouton_5 = tk.Button(racine, text="2")
-bouton_5.grid(row=2, column=0)
+bouton_5 = tk.Button(racine, text="5")
+bouton_5.grid(row=3, column=2)
 
-bouton_6 = tk.Button(racine, text="2")
-bouton_6.grid(row=2, column=0)
+bouton_6 = tk.Button(racine, text="6")
+bouton_6.grid(row=3, column=3)
 
-bouton_7 = tk.Button(racine, text="2")
-bouton_7.grid(row=2, column=0)
+bouton_7 = tk.Button(racine, text="7")
+bouton_7.grid(row=4, column=1)
 
-bouton_8 = tk.Button(racine, text="2")
-bouton_8.grid(row=2, column=0)
+bouton_8 = tk.Button(racine, text="8")
+bouton_8.grid(row=4, column=2)
 
-bouton_9 = tk.Button(racine, text="2")
-bouton_9.grid(row=2, column=0)
+bouton_9 = tk.Button(racine, text="9")
+bouton_9.grid(row=4, column=3)
 
-canvas=tk.Canvas(racine, width=500, height=600, background='black')
-canvas.grid(row=1, column=1, rowspan=3)
+canvas=tk.Canvas(racine, width=100, height=100)
+canvas.grid(row=6, column=4, rowspan=1)
+
+racine.mainloop()
